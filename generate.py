27aeb3cdf39a65ca6168fddf86849208032bcf9e
piqueonmarketer777-png <piqueonmarketer777@gmail.com>
2026-07-14 15:14:18 +0500

    Update generate.py

--- generate.py
+++ generate.py
@@ -2,13 +2,7 @@ from huggingface_hub import InferenceClient
 import streamlit as st
 
 def generate_banner(prompt):
-    # Retrieve the key from Streamlit Secrets
-    api_key = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
+    api_key = st.secrets["HUGGINGFACE_API_TOKEN"]
     client = InferenceClient(api_key=api_key)
-    
-    # Generate the image
-   # Change from FLUX.1-schnell to this:
-image = client.text_to_image(prompt, model="stabilityai/stable-diffusion-2-1")
-    
-    # IMPORTANT: You must return the image!
+    image = client.text_to_image(prompt, model="stabilityai/stable-diffusion-2-1")
     return image
