f48733945754b8607e4ff22876eda0c6f61249f1
piqueonmarketer777-png <piqueonmarketer777@gmail.com>
2026-07-15 12:17:41 +0500

    Update generate.py

--- generate.py
+++ generate.py
@@ -4,5 +4,5 @@ import streamlit as st
 def generate_banner(prompt):
     api_key = st.secrets["HUGGINGFACE_API_TOKEN"]
     client = InferenceClient(api_key=api_key)
-   image = client.text_to_image(prompt, model="runwayml/stable-diffusion-v1-5")
+    image = client.text_to_image(prompt, model="runwayml/stable-diffusion-v1-5")
     return image
