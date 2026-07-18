31ad967e6ca8bb2207af5decde4bc1250fea62ae
piqueonmarketer777-png <piqueonmarketer777@gmail.com>
2026-07-18 13:03:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import streamlit as st
 from generate import generate_banner
 st.set_page_config(page_title="Sajida AI Systems")
-st.title("Sajida AI Systems -  Image Geneartor")
+st.title("Sajida AI Systems - Image Generator")
 st.subheader("Professional  AI Visuals")
-user_prompt = st.text_input("Entre your banner description:")
+user_prompt = st.text_input("Enter your banner description:")
 if st.button("Generate Image"):
     if user_prompt:
         with st.spinner('Generating...please wait.'):
-            image = genearte_banner(user_prompt)
+          image = generate_banner(user_prompt)
             if image:
                 st.image(image, caption="Generated Banner")
                 st.success("Success! Image created.")
