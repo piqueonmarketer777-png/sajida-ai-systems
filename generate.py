ece0413c7df2683f2e767f724be74ee83766fcdb
piqueonmarketer777-png <piqueonmarketer777@gmail.com>
2026-07-18 12:53:55 +0500

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,11 +1,7 @@
 import streamlit as st
 from huggingface_hub import InferenceClient
-
 def generate_banner(prompt):
     api_key = st.secrets["HUGGINGFACE_API_TOKEN"]
-    # This uses the official, simplified client
     client = InferenceClient(api_key=api_key)
-    
-    # We use a public model that is usually allowed
-   image = client.text_to_image(prompt, model="stabilityai/stable-diffusion-xl-base-1.0")
+    image = client.text_to_image(prompt, model="stabilityai/stable-diffusion-xl-base-1.0")
     return image
